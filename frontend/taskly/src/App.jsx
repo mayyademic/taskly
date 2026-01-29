3ccaa47d396d80d6c9f0d5f9b44b1118785cce9d
shrek-007 <myshkovaviktoriia@gmail.com>
2026-01-29 12:42:14 +0100

    login page - adding the user and password inputs

---
+++ frontend/taskly/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import LoginAuth from "./Authentication/login.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginAuth />
+      </header>
+    </div>
+  );
+}
+
+export default App;
