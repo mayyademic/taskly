94c8682616c368e30e4399f9601d0f4b20494c52
Demitskaya, Mayya (I/VE-D81, extern) <Mayya.Demitskaya@donat-it.de>
2026-02-23 14:51:29 +0100

    Backend with Docker

---
+++ backend/taskly-backend/src/main/java/com/example/backend/model/Task.java
@@ -0,0 +1,117 @@
+package com.example.backend.model;
+
+import java.util.StringJoiner;
+
+public class Task {
+    private Integer taskId;
+    private String taskTitle;
+    private String taskText;
+    private String taskStatus;
+    private Integer groupId;
+
+    public Task() {
+    }
+
+    private Task(Builder builder) {
+        setTaskId(builder.taskId);
+        setTaskTitle(builder.taskTitle);
+        setTaskText(builder.taskText);
+        setTaskStatus(builder.taskStatus);
+        setGroupId(builder.groupId);
+    }
+
+    public Integer getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public void setTaskTitle(String taskTitle) {
+        this.taskTitle = taskTitle;
+    }
+
+    public String getTaskText() {
+        return taskText;
+    }
+
+    public void setTaskText(String taskText) {
+        this.taskText = taskText;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Task.class.getSimpleName() + "[", "]")
+                .add("taskId=" + taskId)
+                .add("taskTitle='" + taskTitle + "'")
+                .add("taskText='" + taskText + "'")
+                .add("taskStatus='" + taskStatus + "'")
+                .add("groupId=" + groupId)
+                .toString();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private Integer taskId;
+        private String taskTitle;
+        private String taskText;
+        private String taskStatus;
+        private Integer groupId;
+
+        public Builder() {
+        }
+
+        public Builder withTaskId(Integer val) {
+            taskId = val;
+            return this;
+        }
+
+        public Builder withTaskTitle(String val) {
+            taskTitle = val;
+            return this;
+        }
+
+        public Builder withTaskText(String val) {
+            taskText = val;
+            return this;
+        }
+
+        public Builder withTaskStatus(String val) {
+            taskStatus = val;
+            return this;
+        }
+
+        public Builder withGroupId(Integer val) {
+            groupId = val;
+            return this;
+        }
+
+        public Task build() {
+            return new Task(this);
+        }
+    }
+}
